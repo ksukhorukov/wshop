04ff1399586d8b7d1ad22661f9d597894beb3c00
Kirill <kirill.suhorukov@gmail.com>
2019-02-22 19:29:18 +0300

    redirection to admin panel after sign_in, admin panel view enhancement

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,21 @@
 class Purchase < ApplicationRecord
+	validate :card_truncated_validation
+	validates :email, :card_truncated, presence: true
+
+	before_save :truncate_card
+
 	belongs_to :cart
 	belongs_to :shop
 
-	validates :email, :card_truncated, presence: true
+	private 
+
+	def card_truncated_validation
+		card_number = card_truncated.split.join
+		errors.add(:purchase, 'Invalid card') unless card_number.match(/\d+/) 
+		errors.add(:purchase, 'Invalid card') if card_number.size != 16
+	end
+
+	def truncate_card
+		card_truncated = card_truncated.slice(12, 4)
+	end
 end
